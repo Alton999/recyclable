c5b2d8353853e8dafd6e07b521b7379b76781819
Alton <alton_ong@live.com.sg>
2022-10-28 14:54:10 +1100

    final commit before testing

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,22 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { useState, useEffect } from "react";
 
 import Step1 from "./introStep";
 import Step2 from "./scanningStep";
+
+const firebaseConfig = {
+	apiKey: "AIzaSyDMlpK8ieUVC9EKE7LeLEo20GNMXpHf4qk",
+	authDomain: "recyclablemobile.firebaseapp.com",
+	projectId: "recyclablemobile",
+	storageBucket: "recyclablemobile.appspot.com",
+	messagingSenderId: "999356890366",
+	appId: "1:999356890366:web:7bd4257fa5660f3564b9af",
+	measurementId: "G-262FFBE691"
+};
+
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
 
 const Home = () => {
 	const [step, setStep] = useState(1);
